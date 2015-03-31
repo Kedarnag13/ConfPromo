6262e84f664b4252ca0ec903ecdacd60582e2d63
KJsharmila <sharmila437@gmail.com>
2015-03-31 12:01:27 +0530

    ui fixed in admin part

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  private
+  def admin_login
+    unless session[:user_id] == 7
+       redirect_to root_path
+     end
+   end
 end
